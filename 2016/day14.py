10b197a7e95f9e2dfea5244cc565c6c4645b18e5
lemurey <lee.murray@gmail.com>
2016-12-14 09:26:45 -0800

    clean up and add timing to day14

--- 2016/day14.py
+++ 2016/day14.py
@@ -1,9 +1,9 @@
-import md5, re
-
-c1 = re.compile(r'(\w)\1\1')
+import re
+from hashlib import md5
+from time import time
 
 def compute_hash(string, count):
-    return md5.new('{}{}'.format(string, count)).hexdigest()
+    return md5('{}{}'.format(string, count)).hexdigest()
 
 
 def advanced_hashes(string, count):
@@ -13,38 +13,54 @@ def advanced_hashes(string, count):
     return temp_hash
 
 
+def timeit(function):
+    def timed(*args, **kwargs):
+        s_t = time()
+        result = function(*args, **kwargs)
+        e_t = time()
+        out = '{} {} took {:.2f} sec'
+        print out.format(function.__name__, kwargs, e_t - s_t)
+        return result
+    return timed
+
+
+@timeit
 def get_results(instructions, part2=False):
     hash_checks = {}
     keys = []
     count = 0
+    checker = re.compile(r'(\w)\1\1')
+    to_remove = -1
     while len(keys) < 64:
-        to_remove = []
-        skip = False
+
         if part2:
             hashed = advanced_hashes(instructions, count)
         else:
             hashed = compute_hash(instructions, count)
-        for k, regex in hash_checks.iteritems():
-            if count <= k + 1000 and count > k:
-                if regex.search(hashed):
-                    keys.append(k)
-            elif count > k + 1000:
-                to_remove.append(k)
-        for index in to_remove:
-            del hash_checks[index]
-        match = c1.search(hashed)
+
+        match = checker.search(hashed)
         if match:
             string = r'({})\1{{4}}'.format(match.group(1))
             hash_checks[count] = re.compile(string)
-        count += 1
-    return sorted(keys)[63]
 
+        for k, regex in hash_checks.iteritems():
+            if k < count and count <= k + 1000:
+                if regex.search(hashed):
+                    keys.append(k)
+            elif count > k + 1000:
+                to_remove = k
+
+        if to_remove in hash_checks:
+            del hash_checks[to_remove]
+
+        count += 1
+        
+    return len(keys), count, sorted(keys)[63]
 
 
 if __name__ == '__main__':
     with open('instructions_day14.txt', 'r') as f:
         instructions = f.read().strip()
-    print get_results(instructions)
-    print get_results(instructions, True)
-    # print get_results('abc')
 
+    print get_results(instructions, part2=False)
+    print get_results(instructions, part2=True)
