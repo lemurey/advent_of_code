18cdb05a816bb673b32a94c717e798fdc3fbe2a6
lemurey <lee.murray@gmail.com>
2017-01-26 14:04:54 -0800

    add boilerplate file

---
+++ boilerplate.py
@@ -0,0 +1,12 @@
+from utilities import get_data, timeit, add_points
+
+
+@timeit
+def get_results(data, part2=False):
+    return
+
+
+if __name__ == '__main__':
+    data = get_data(day=)
+    print get_results(data, part2=False)
+    # print get_results(data, part2=True)
