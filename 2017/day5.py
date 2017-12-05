87be8433926f4201cc5e113dbcfd21cb4efef892
lemurey <lee.murray@gmail.com>
2017-12-04 21:28:33 -0800

    add day 5

---
+++ 2017/day5.py
@@ -0,0 +1,33 @@
+from aoc_utilities import get_instructions
+import os
+
+
+def parse_instructions(data):
+    output = {}
+    for i, line in enumerate(data.split()):
+        output[i] = int(line)
+    return output
+
+
+
+def get_answer(data, mode):
+    instructions = parse_instructions(data)
+    index = 0
+    steps = 0
+    while index in instructions:
+        steps += 1
+        jump = instructions[index]
+        offset = 1
+        if mode != 'part1' and jump >= 3:
+            offset = -1
+        instructions[index] += offset
+        index += jump
+    return steps
+
+
+
+if __name__ == '__main__':
+    day = int(os.path.basename(__file__).split('.')[0].split('y')[1])
+    inputs = get_instructions(day)
+    print(get_answer(inputs, mode='part1'))
+    print(get_answer(inputs, mode='part2'))
