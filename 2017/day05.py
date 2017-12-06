623ba37a8d33bf8e19702466d512e48695ad465c
Lee Murray <lee.murray@galvanize.com>
2017-12-06 13:09:24 -0800

    update template, modify day 5

--- 2017/day05.py
+++ 2017/day05.py
@@ -10,17 +10,22 @@ def parse_instructions(data):
     return output
 
 
+def get_offset(part2, jump):
+    if part2 and jump > 2:
+        return -1
+    else:
+        return 1
+
+
 @timeit
-def get_answer(data, mode):
+def get_answer(data, part2=False):
     instructions = parse_instructions(data)
     index = 0
     steps = 0
     while index in instructions:
         steps += 1
         jump = instructions[index]
-        offset = 1
-        if mode != 'part1' and jump >= 3:
-            offset = -1
+        offset = get_offset(part2, jump)
         instructions[index] += offset
         index += jump
     return steps
@@ -29,5 +34,5 @@ def get_answer(data, mode):
 if __name__ == '__main__':
     day = int(os.path.basename(__file__).split('.')[0].split('y')[1])
     inputs = get_instructions(day)
-    print(get_answer(inputs, mode='part1'))
-    print(get_answer(inputs, mode='part2'))
+    print(get_answer(inputs, part2=False))
+    print(get_answer(inputs, part2=True))
--- 2017/template.py
+++ 2017/template.py
@@ -2,12 +2,12 @@ from aoc_utilities import get_instructions
 import os
 
 
-def get_answer(data, mode):
+def get_answer(data, part2=False):
     pass
 
 
 if __name__ == '__main__':
     day = int(os.path.basename(__file__).split('.')[0].split('y')[1])
     inputs = get_instructions(day)
-    print(get_answer(inputs, mode='part1'))
-    print(get_answer(inputs, mode='part2'))
+    print(get_answer(inputs, part2=False))
+    print(get_answer(inputs, part2=True))
