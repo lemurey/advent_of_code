13880bb14d665c340d21a0c44ce17dcb4814bc62
lemurey <lee.murray@gmail.com>
2017-12-10 13:09:36 -0800

    redo day10 with functions

--- 2017/day10.py
+++ 2017/day10.py
@@ -1,88 +1,72 @@
 from aoc_utilities import get_instructions
 from functools import reduce
+from utilities import timeit
 import os
 
 
-class Knothash:
-    def __init__(self, length=256):
-        self.list = list(range(length))
-        self.skip_size = 0
-        self.current_position = 0
+def reverse(array, length, position):
+    if position + length >= len(array):
+        new_vals = array[position:]
+        new_vals += array[:(position + length) % 256]
+    else:
+        new_vals = array[position:(position + length)]
 
-    def _get_index(self, index):
-        return (self.current_position + index) % len(self.list)
+    new_vals = new_vals[::-1]
 
-    def _reverse(self, length):
-        if self.current_position + length >= len(self.list):
-            new_vals = self.list[self.current_position:]
-            new_vals += self.list[:self._get_index(length)]
-        else:
-            new_vals = self.list[self.current_position:self._get_index(length)]
+    for index in range(length):
+        replace = (position + index) % 256
 
-        assert len(new_vals) == length, '{}'.format(self.skip_size)
-
-        new_vals = new_vals[::-1]
-
-        for index in range(length):
-            replace = self._get_index(index)
-            self.list[replace] = new_vals[index]
-
-    def __call__(self, length):
-        self._reverse(length)
-        self.current_position = self._get_index(length + self.skip_size)
-        self.skip_size += 1
-
-    def check_result(self, part2=False):
-        if part2:
-            return self._make_dense_hash()
-        return self.list[0] * self.list[1]
-
-    def _make_dense_hash(self):
-        hash_list = []
-        prev_index = 0
-        for index in range(16, len(self.list) + 1, 16):
-            vals = self.list[prev_index:index]
-            hash_list.append(self._xor(vals))
-            prev_index = index
-        return ''.join('{:02X}'.format(d) for d in hash_list).lower()
-
-    def _xor(self, sub_list):
-        return reduce(lambda x, y: x ^ y, sub_list)
+        array[replace] = new_vals[index]
 
 
-def print_list(kh):
-    p_string = ''
-    for i, val in enumerate(kh.list):
-        if i == kh.current_position:
-            o = '[{}]'.format(val)
-        else:
-            o = str(val)
-        p_string += '{} '.format(o)
-    return p_string
+def many_xor(array):
+    return reduce(lambda x, y: x ^ y, array)
+
+
+def knothash(data):
+    sequence = update_data(data)
+    vals = list(range(256))
+    position = 0
+    skip_size = 0
+    for _ in range(64):
+        for length in sequence:
+            reverse(vals, length, position)
+            position = (position + length + skip_size) % 256
+            skip_size += 1
+    prev_index = 0
+    hash_list = []
+    for index in range(16, 256 + 1, 16):
+        sub_list = vals[prev_index:index]
+        prev_index = index
+        hash_list.append(many_xor(sub_list))
+    return ''.join('{:02X}'.format(d) for d in hash_list).lower()
 
 
 def update_data(data):
     output = []
     for character in data:
         output.append(ord(character))
-    output += [17, 31, 73, 47, 23]
-    output_string = ('{},' * len(output)).strip(',')
-    return output_string.format(*output)
+    output.extend([17, 31, 73, 47, 23])
+    return output
 
 
+def run_part_1(data):
+    vals = list(range(256))
+    position = 0
+    skip_size = 0
+    for entry in data.split(','):
+        length = int(entry)
+        reverse(vals, length, position)
+        position = (position + length + skip_size) % 256
+        skip_size += 1
+    return vals[0] * vals[1]
+
+
+@timeit
 def get_answer(data, part2=False):
-    kh = Knothash()
-    if part2:
-        data = update_data(data)
-        num_rounds = 64
-    else:
-        num_rounds = 1
-    for _ in range(num_rounds):
-        for entry in data.split(','):
-            length = int(entry)
-            kh(length)
-
-    return kh.check_result(part2)
+    if not part2:
+        return run_part_1(data)
+    return knothash(data)
 
 
 if __name__ == '__main__':
