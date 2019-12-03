33ec6f0657be1f00c3983b5c4a6d82bfbc2f195b
lemurey <lee.murray@gmail.com>
2019-12-02 20:54:44 -0800

    finished day 02

--- 2019/day02.py
+++ 2019/day02.py
@@ -1,9 +1,62 @@
 from aoc_utilities import get_instructions
 import os
 
+class Intcode():
+    def __init__(self, program):
+        self.program = program
+        self.orig = [x for x in self.program]
+
+    def code_01(self, index):
+        a = self.program[index + 1]
+        b = self.program[index + 2]
+        c = self.program[index + 3]
+        self.program[c] = self.program[a] + self.program[b]
+
+    def code_02(self, index):
+        a = self.program[index + 1]
+        b = self.program[index + 2]
+        c = self.program[index + 3]
+        self.program[c] = self.program[a] * self.program[b]
+
+    def run(self):
+        cur_ind = 0
+        while True:
+            which = self.program[cur_ind]
+            if which == 1:
+                self.code_01(cur_ind)
+            elif which == 2:
+                self.code_02(cur_ind)
+            elif which == 99:
+                return
+            else:
+                raise ValueError('incorrect value')
+            cur_ind += 4
+
+    def reset(self, noun, verb):
+        self.program = [x for x in self.orig]
+        self.program[1] = noun
+        self.program[2] = verb
+
+
+def parse_inputs(data):
+    return list(map(int, data[0].split(',')))
+
 
 def get_answer(data, part2=False):
-    pass
+    inputs = parse_inputs(data)
+    inputs[1] = 12
+    inputs[2] = 2
+    comp = Intcode(inputs)
+    if part2:
+        for i in range(100):
+            for j in range(100):
+                comp.reset(i, j)
+                comp.run()
+                if comp.program[0] == 19690720:
+                    return 100 * i + j
+    else:
+        comp.run()
+        return comp.program[0]
 
 
 if __name__ == '__main__':
