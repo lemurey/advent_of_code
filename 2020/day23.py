89a9c261f6722d6f3e5e792e5955d544ef2b3d44
lemurey <lee.murray@gmail.com>
2020-12-24 10:23:44 -0800

    refactor day23

--- 2020/day23.py
+++ 2020/day23.py
@@ -3,101 +3,114 @@ from pathlib import Path
 
 
 class Cup:
-    def __init__(self, value):
+    def __init__(self, value, right=None):
         self.value = value
-        self.left = None
-        self.right = None
-
-    def __repr__(self):
-        return f'{self.value}'
+        self.right = right
 
     def __str__(self):
-        return self.__repr__()
+        return f'{self.value}'
 
 
-def setup(data, part2=False):
-    first = None
-    last = None
-    arr = [int(x) for x in data[0]]
+class Cups:
+    def __init__(self, start, max_value=None):
+        self._setup(start, max_value)
+        self._write_start = int(start[0])
 
-    if part2:
-        cups = [None] * 1000001
-    else:
-        cups = [None] * (len(arr) + 1)
-    max_value = 0
-    for val in arr:
-        cup = Cup(val)
-        max_value = max(val, max_value)
-        cups[val] = cup
+    def _setup(self, start, max_value):
+        arr = [int(x) for x in start]
+        if max_value is None:
+            max_value = max(arr)
 
-        if first is None:
-            first = cup
-        if last is not None:
+        self.cups = [None] * (max_value + 1)
+
+        first = Cup(arr[0])
+        last = first
+        self.cups[arr[0]] = first
+
+        current = 0
+
+        for val in arr[1:]:
+            if val > current:
+                current = val
+            cup = Cup(val)
+            self.cups[val] = cup
             last.right = cup
-            last.left = last
-        last = cup
-
-    if part2:
-        while max_value < 1000000:
-            max_value += 1
-            cup = Cup(max_value)
-
-            cups[max_value] = cup
-            last.right = cup
-            cup.left = last
             last = cup
 
-    last.right = first
-    first.left = last
+        while self.cups[-1] is None:
+                current += 1
+                cup = Cup(current)
+                self.cups[current] = cup
 
+                last.right = cup
+                last = cup
 
-    return cups, first, max_value
+        last.right = first
 
+        self.max_value = max_value
+        self.current = first
 
-def run_game(cups, current, max_value, rounds):
-    for round_num in range(rounds):
-        if round_num % 2500000 == 0:
-            print(f'On round {round_num}')
-
-        # get next 3 values
-        c1 = current.right
+    def run_round(self):
+        # get the next three cups in ring
+        c1 = self.current.right
         c2 = c1.right
         c3 = c2.right
         store = {c1.value, c2.value, c3.value}
 
-        # skip over the 3 removed values
-        current.right = c3.right
+        # find destination
+        dest_value = self.current.value
+        destination = None
+        while destination is None:
+            dest_value -= 1
+            if dest_value < 1:
+                dest_value = self.max_value
+            if dest_value not in store:
+                destination = self.cups[dest_value]
 
-        # find the destination
-        destination = current.value
-        while True:
-            destination -= 1
-            if destination < 1:
-                destination = max_value
-            if destination not in store:
-                destination = cups[destination]
-                break
-
-        # put the removed values back in the ring
+        # snip ring
+        self.current.right = c3.right
+        # right of destination is now right of c3
         c3.right = destination.right
-        c3.right.left = c3
+        # right of destination is now c1
         destination.right = c1
-        c1.left = current
+        # move forward one in ring
+        self.current = self.current.right
 
-        # advance 1
-        current = current.right
-    return cups
+    def __getitem__(self, index):
+        return self.cups[index]
 
+    def _write(self, cup):
+        if cup == self.current:
+            return f'({cup}) '
+        return f'{cup} '
+
+    def __str__(self):
+        start = self.cups[self._write_start]
+        out = self._write(start)
+        current = start.right
+        while current != start:
+            out += self._write(current)
+            current = current.right
+        return out
+
+
+def run_game(cups, rounds):
+    for round_num in range(rounds):
+        if round_num % 2500000 == 0 and round_num != 0:
+            print(f'On round {round_num}')
+        cups.run_round()
 
 def get_answer(data, part2=False):
-
-    cups, start, max_value = setup(data, part2)
+    if part2:
+        cups = Cups(data[0], 1000000)
+    else:
+        cups = Cups(data[0])
 
     if part2:
-        cups = run_game(cups, start, max_value, rounds=10000000)
+        run_game(cups, 10000000)
         return cups[1].right.value * cups[1].right.right.value
     else:
-        cups = run_game(cups, start, max_value, rounds=100)
+        run_game(cups, 100)
         out = ''
         current = cups[1].right
         while current != cups[1]:
