4af0f68304ebcffb5068a5759b60701f8ccea2fc
lemurey <lee.murray@gmail.com>
2022-12-09 21:31:02 -0800

    add day10 2022

---
+++ 2022/day10.py
@@ -0,0 +1,201 @@
+from aoc_utilities import get_instructions
+from pathlib import Path
+
+
+def run_instructions(instructions):
+    cycle = 0
+    x = 1
+    log = []
+    countdown = 0
+    buff = 0
+    pixel = 0
+    output = ''
+    while cycle < 240:
+        if countdown == 0:
+            inst = next(instructions)
+            x += buff
+            sprite = set(range(x-1, x+2))
+            if inst == 'noop':
+                countdown = 1
+                buff = 0
+            else:
+                countdown = 2
+                buff = int(inst.split()[-1])
+
+        if pixel in sprite:
+            output += '#'
+        else:
+            output += '.'
+
+        cycle += 1
+        countdown -= 1
+        pixel += 1
+        if (cycle - 20) % 40 == 0:
+            log.append(x * cycle)
+
+        if cycle % 40 == 0:
+            output += '\n'
+            pixel = 0
+
+    return log, output
+
+
+def get_answer(data, part2=False):
+    log, output = run_instructions(iter(data))
+    print(sum(log))
+    return output
+
+
+if __name__ == '__main__':
+    p = Path(__file__).absolute()
+    year = p.parent.stem
+    day = int(p.stem.split('y')[1])
+    inputs = get_instructions(year, day)
+#     inputs = '''addx 15
+# addx -11
+# addx 6
+# addx -3
+# addx 5
+# addx -1
+# addx -8
+# addx 13
+# addx 4
+# noop
+# addx -1
+# addx 5
+# addx -1
+# addx 5
+# addx -1
+# addx 5
+# addx -1
+# addx 5
+# addx -1
+# addx -35
+# addx 1
+# addx 24
+# addx -19
+# addx 1
+# addx 16
+# addx -11
+# noop
+# noop
+# addx 21
+# addx -15
+# noop
+# noop
+# addx -3
+# addx 9
+# addx 1
+# addx -3
+# addx 8
+# addx 1
+# addx 5
+# noop
+# noop
+# noop
+# noop
+# noop
+# addx -36
+# noop
+# addx 1
+# addx 7
+# noop
+# noop
+# noop
+# addx 2
+# addx 6
+# noop
+# noop
+# noop
+# noop
+# noop
+# addx 1
+# noop
+# noop
+# addx 7
+# addx 1
+# noop
+# addx -13
+# addx 13
+# addx 7
+# noop
+# addx 1
+# addx -33
+# noop
+# noop
+# noop
+# addx 2
+# noop
+# noop
+# noop
+# addx 8
+# noop
+# addx -1
+# addx 2
+# addx 1
+# noop
+# addx 17
+# addx -9
+# addx 1
+# addx 1
+# addx -3
+# addx 11
+# noop
+# noop
+# addx 1
+# noop
+# addx 1
+# noop
+# noop
+# addx -13
+# addx -19
+# addx 1
+# addx 3
+# addx 26
+# addx -30
+# addx 12
+# addx -1
+# addx 3
+# addx 1
+# noop
+# noop
+# noop
+# addx -9
+# addx 18
+# addx 1
+# addx 2
+# noop
+# noop
+# addx 9
+# noop
+# noop
+# noop
+# addx -1
+# addx 2
+# addx -37
+# addx 1
+# addx 3
+# noop
+# addx 15
+# addx -21
+# addx 22
+# addx -6
+# addx 1
+# noop
+# addx 2
+# addx 1
+# noop
+# addx -10
+# noop
+# noop
+# addx 20
+# addx 1
+# addx 2
+# addx 2
+# addx -6
+# addx -11
+# noop
+# noop
+# noop'''.split('\n')
+    print(get_answer(inputs, part2=False))
+    # print(get_answer(inputs, part2=True))
